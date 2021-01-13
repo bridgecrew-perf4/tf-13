a7a5f6c5a979565ddd55bbcf846027fdca66ac45
Muhammad Danish Khan <mdk2948@gmail.com>
2021-01-13 22:35:46 +0500

    First commit

---
+++ SG.tf
@@ -0,0 +1,20 @@
+esource "aws_security_group" "danish-sg" {
+  name ="danish-sg"
+  description = "secuirty group for ec2 machine"
+
+ingress  {
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = [ "0.0.0.0/0" ]
+  description = "ssh port"
+  } 
+
+egress   {
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = [ "0.0.0.0/0" ]
+    } 
+
+}
